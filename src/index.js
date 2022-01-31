28a4610de8247a79825a0ac552975d058a5e8db9
Alexey Mikhailov <56613404+amikha1lov@users.noreply.github.com>
2022-01-31 17:54:06 +0300

    desktop version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 import './style.css';
+
+document.addEventListener("DOMContentLoaded", () => {
+    new SlimSelect({
+        select: '#brand__select',
+        showSearch: false,
+        onChange: (info) => {
+            console.log(info)
+        },
+        placeholder:'Марка'
+    })
+    new SlimSelect({
+        select: '#model__select',
+        showSearch: false,
+        placeholder:'Модель'
+    })
+    new SlimSelect({
+        select: '#body_year__select',
+        showSearch: false,
+        placeholder:'Год кузова'
+    })
+    new SlimSelect({
+        select: '#glass_type__select',
+        showSearch: false,
+        placeholder:'Тип стекла'
+    })
+    new SlimSelect({
+        select: '#service__select',
+        showSearch: false,
+        placeholder:'Услуга'
+    })
+
+    var gallery = new Swiper(".gallery", {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    var reviews = new Swiper(".reviews", {
+        scrollbar: {
+            el: ".swiper-scrollbar-reviews",
+
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+
+
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   mode: 'jit',
   content: ['./dist/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'selects': '0px 30px 59px 0px #35353512;',
+        'about-badge': '0px 30px 59px rgba(53, 53, 53, 0.07);',
+        'services-card': '0px 30px 49px #B43331;',
+      },
+      backgroundImage: {
+        'hero': "url('/dist/img/car_bg.png')",
+      },
+    },
   },
   plugins: [],
 }
