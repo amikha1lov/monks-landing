d67b474514c270dffa928826d508bf605066edac
Alexey Mikhailov <56613404+amikha1lov@users.noreply.github.com>
2022-02-01 17:52:04 +0300

    правки и форма

--- src/index.js
+++ src/index.js
@@ -66,5 +66,13 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    MicroModal.init({
+        openTrigger: 'data-custom-open',
+        closeTrigger: 'data-custom-close',
+        disableScroll: true,
+        disableFocus: true,
+        awaitOpenAnimation: true,
+        awaitCloseAnimation: true
+    })
 
 });
